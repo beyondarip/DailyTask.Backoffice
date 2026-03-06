e56dd2d8ff98873aefd8f451a24599ece44c467d
beyondarip <ari2331280@itpln.ac.id>
2026-03-06 13:26:42 +0700

    ejs

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ if (fs.existsSync(caPath)) {
 const pool = new Pool(poolConfig);
 
 // ─── MIDDLEWARE ────────────────────────────────────────────────────────────────
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
